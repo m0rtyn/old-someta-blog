3a23194e416ab0683f78ef2cba45e9b67273fa53
martyns0n <zogacc@gmail.com>
2018-10-03 07:49:49 +0300

    upload on new repo

--- assets/js/main.js
+++ assets/js/main.js
@@ -58,3 +58,15 @@ const msnry = new Masonry( grid, {
 //   // layout Masonry after each image loads
 //   msnry.layout();
 // });
+
+
+const root = document.documentElement;
+let rootHeight = root.clientHeight;
+console.log(rootHeight);
+
+console.log(document.body.scrollTop || document.documentElement.scrollTop, rootHeight * 0.3);
+window.addEventListener('scroll', () => {
+  if (document.body.scrollTop || document.documentElement.scrollTop === rootHeight * 0.3) {
+    console.log('Очевидно фич больше')
+  }
+})
