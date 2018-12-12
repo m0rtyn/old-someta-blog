a257ba1ad1edfc31437da3b60c8e0bac288ad942
martyns0n <zogacc@gmail.com>
2018-12-12 10:04:08 +0300

    add async rows loading

--- src/assets/js/booksheets.js
+++ src/assets/js/booksheets.js
@@ -1,14 +1,25 @@
-const xhr = new XMLHttpRequest()
+const fillTable = (bookArray) => {
+  const booksRoot = document.getElementById('books')
+  const table = document.getElementById('booksTable')
+  const tbody = document.createElement('TBODY')
+  const thead = document.createElement('THEAD')
+  table.appendChild(thead)
+  table.appendChild(tbody)
 
-xhr.open(
-  'GET',
-  'https://script.google.com/macros/s/AKfycbyncOZcKRu3_shSLYYescpIelkontkjdTuCRONzfXIXK0Jl5Vqb/exec',
-  false
-)
+  bookArray.forEach((row, i, arr) => {
+    setTimeout(() => {
+      let tr = document.createElement('TR')
+      row.forEach((cell, j, arr) => {
+        const cellText = document.createTextNode(bookArray[i][j])
+        i === 0 ?
+          createTableCell(tr, cellText, j, 'TH') :
+          createTableCell(tr, cellText, j, 'TD')
+      })
+      i === 0 ? thead.appendChild(tr) : tbody.appendChild(tr)
+    }, 100 * i);
+  })
+}
 
-xhr.send()
-
-const errorMessage = xhr.status + ': ' + xhr.statusText
 const createTableCell = (tr, cellText, index, type) => {
   let tableCell = document.createElement(type)
   tr.appendChild(tableCell)
@@ -28,37 +39,28 @@ const createTableCell = (tr, cellText, index, type) => {
       break
     case 4:
       tableCell.classList.add('book-status')
-      cellText.data == '✔'
-        ? tableCell.classList.add('status-done')
-        : cellText.data == '►'
-        ? tableCell.classList.add('status-in-progress')
-        : cellText.data == '■'
-        ? tableCell.classList.add('status-stopped')
-        : null
+      setBookStatus(tableCell, cellText.data)
       break
   }
 }
 
-const fillTable = () => {
-  const bookSheet = JSON.parse(xhr.response).result
-  const booksRoot = document.getElementById('books')
-  const table = document.getElementById('booksTable')
-  const tbody = document.createElement('TBODY')
-  const thead = document.createElement('THEAD')
-
-  bookSheet.forEach((row, i, arr) => {
-    let tr = document.createElement('TR')
-    i === 0 ? thead.appendChild(tr) : tbody.appendChild(tr)
-    row.forEach((cell, j, arr) => {
-      const cellText = document.createTextNode(bookSheet[i][j])
-      i === 0
-        ? createTableCell(tr, cellText, j, 'TH')
-        : createTableCell(tr, cellText, j, 'TD')
-    })
-  })
-
-  table.appendChild(thead)
-  table.appendChild(tbody)
+const setBookStatus = (cell, text) => {
+  switch (text) {
+    case '✔':
+      cell.classList.add('status-done')
+      break
+    case '►':
+      cell.classList.add('status-in-progress')
+      break
+    case '■':
+      cell.classList.add('status-stopped')
+    default:
+      break
+  }
 }
 
-xhr.status != 200 ? console.log(errorMessage) : fillTable()
+fetch('https://script.google.com/macros/s/AKfycbyncOZcKRu3_shSLYYescpIelkontkjdTuCRONzfXIXK0Jl5Vqb/exec')
+  .then((response) => response.json())
+  .then(({
+    result
+  }) => fillTable(result))
