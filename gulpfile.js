316f388e9eed5575ac5efc1843f414d9da567d34
martyns0n <zogacc@gmail.com>
2018-12-16 19:49:32 +0300

    add error throwing for js task

--- gulpfile.js
+++ gulpfile.js
@@ -1,118 +1,113 @@
-'use strict'
+const gulp = require('gulp');
+const pug = require('gulp-pug');
+const sass = require('gulp-sass');
+const plumber = require('gulp-plumber');
+const postcss = require('gulp-postcss');
+const svgmin = require('gulp-svgmin');
+const rename = require('gulp-rename');
+const browserSync = require('browser-sync').create();
+const del = require('del');
+const htmlmin = require('gulp-htmlmin');
+const babel = require('gulp-babel');
+const postcssFixes = require('postcss-fixes');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
 
-const gulp = require('gulp')
-const pug = require('gulp-pug')
-const sass = require('gulp-sass')
-const concat = require('gulp-concat')
-const plumber = require('gulp-plumber')
-const postcss = require('gulp-postcss')
-const svgmin = require('gulp-svgmin')
-const rename = require('gulp-rename')
-const browserSync = require('browser-sync').create()
-const del = require('del')
-const htmlmin = require('gulp-htmlmin')
-const babel = require('gulp-babel')
 
-gulp.task('style', function () {
+gulp.task('style', () => {
   gulp
     .src('src/assets/styles/main.scss')
     .pipe(plumber())
     .pipe(sass())
     .pipe(
       postcss([
-        require('postcss-fixes'),
-        require('autoprefixer'),
-        require('cssnano')({
+        postcssFixes,
+        autoprefixer,
+        cssnano({
           safe: true,
           calc: false,
         }),
-      ])
+      ]),
     )
     .pipe(rename('style.min.css'))
     .pipe(gulp.dest('public/assets/styles'))
-    .pipe(browserSync.stream())
-})
+    .pipe(browserSync.stream());
+});
 
-gulp.task('pug', function () {
-  return gulp
-    .src('src/markup/pages/**/*.pug')
-    .pipe(plumber())
-    .pipe(
-      pug({
-        pretty: true,
-      })
-    )
-    .pipe(htmlmin({
-      collapseWhitespace: true
-    }))
-    .pipe(gulp.dest('public'))
-    .pipe(browserSync.stream())
-})
+gulp.task('pug', () => gulp
+  .src('src/markup/pages/**/*.pug')
+  .pipe(plumber())
+  .pipe(
+    pug({
+      pretty: true,
+    }),
+  )
+  .pipe(htmlmin({
+    collapseWhitespace: true,
+  }))
+  .pipe(gulp.dest('public'))
+  .pipe(browserSync.stream()));
 
-gulp.task('js', function () {
-  return gulp
-    .src('src/assets/js/*.js')
-    .pipe(babel({
-      presets: ['@babel/env']
-    }))
-    .pipe(gulp.dest('public/assets/js'))
-    .pipe(browserSync.stream())
-})
+const handleError = (error) => {
+  console.log(error.toString());
+  this.emit('end');
+};
 
-gulp.task('images', function () {
-  return gulp
-    .src('src/assets/img/**/**/*.{png,jpg,gif}')
-    .pipe(gulp.dest('public/assets/img'))
-})
+gulp.task('js', () => gulp
+  .src('src/assets/js/*.js')
+  .pipe(babel({
+    presets: ['@babel/env'],
+  }))
+  .on('error', handleError)
+  .pipe(gulp.dest('public/assets/js'))
+  .pipe(browserSync.stream()));
 
-gulp.task('svg', function () {
-  return gulp
-    .src('src/**/**/**/*.svg')
-    .pipe(svgmin())
-    .pipe(gulp.dest('public/'))
-})
+gulp.task('images', () => gulp
+  .src('src/assets/img/**/**/*.{png,jpg,gif}')
+  .pipe(gulp.dest('public/assets/img')));
 
-gulp.task('serve', function () {
+gulp.task('svg', () => gulp
+  .src('src/**/**/**/*.svg')
+  .pipe(svgmin())
+  .pipe(gulp.dest('public/')));
+
+gulp.task('serve', () => {
   browserSync.init({
     server: 'public',
     notify: true,
     open: false,
-  })
+  });
 
-  gulp.watch('src/**/**/*.scss', ['style'])
-  gulp.watch('src/markup/**/**/**/**/*.pug', ['pug'])
-  gulp.watch('src/assets/img/**/*.*', ['images'])
-  gulp.watch('src/assets/js/**/*.js', ['js'])
-})
+  gulp.watch('src/**/**/*.scss', ['style']);
+  gulp.watch('src/markup/**/**/**/**/*.pug', ['pug']);
+  gulp.watch('src/assets/img/**/*.*', ['images']);
+  gulp.watch('src/assets/js/**/*.js', ['js']);
+});
 
-gulp.task('copy', function () {
-  return gulp
-    .src(
-      [
-        './src/assets/fonts/**/*.{woff,woff2}',
-        './src/assets/video/**/*.mp4',
-        './src/shows/**',
-      ], {
-        base: './src'
-      }
-    )
-    .pipe(gulp.dest('./public/'))
-})
+gulp.task('copy', () => gulp
+  .src(
+    [
+      './src/assets/fonts/**/*.{woff,woff2}',
+      './src/assets/video/**/*.mp4',
+      './src/shows/**',
+    ], {
+      base: './src',
+    },
+  )
+  .pipe(gulp.dest('./public/')));
 
-gulp.task('clean', function () {
-  return del.sync([
-    'public/**',
-    '!public',
-    '!public/*',
-    '!public/assets',
-    '!public/assets/video/**',
-    '!public/assets/fonts/**',
-    'public/assets/img/**',
-    '!public/shows',
-    '!public/shows/motivation/**',
-    '!public/shows/255-shades-of-gray/**',
-  ])
-})
+gulp.task('clean', () => del.sync([
+  'public/**',
+  '!public',
+  '!public/*',
+  '!public/assets',
+  '!public/assets/video/**',
+  '!public/assets/fonts/**',
+  'public/assets/img/**',
+  '!public/shows',
+  '!public/shows/motivation/**',
+  '!public/shows/255-shades-of-gray/**',
+]));
 
 gulp.task('default', [
   'clean',
@@ -123,5 +118,5 @@ gulp.task('default', [
   'images',
   'svg',
   'serve',
-])
-gulp.task('build', ['clean', 'copy', 'pug', 'style', 'js', 'images', 'svg'])
+]);
+gulp.task('build', ['clean', 'copy', 'pug', 'style', 'js', 'images', 'svg']);
