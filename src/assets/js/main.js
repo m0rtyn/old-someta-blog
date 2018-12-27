e8cb14ad00f9e5cf5eaf12c289ea000ffe054532
martyns0n <zogacc@gmail.com>
2018-12-27 11:10:02 +0300

    fixes for navigation on devices

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,25 +1,23 @@
 /* eslint-env browser */
 
-// imagesLoaded( grid ).on( 'progress', function() {
-//   // layout Masonry after each image loads
-//   msnry.layout();
-// });
-
+const header = document.getElementById('header');
 const nav = document.getElementById('nav');
 const burger = document.getElementById('burger');
 
 if (window.outerWidth < 500) {
   setTimeout(() => {
     burger.classList.add('slip-appearance');
-  }, 2000);
+  }, 1300);
 
   document.addEventListener('click', (evt) => {
-    if (nav.contains(evt.target)) {
+    if (header.contains(evt.target)) {
       nav.classList.add('slip-appearance');
       burger.classList.remove('slip-appearance');
+      burger.style.display = 'none';
     } else {
       nav.classList.remove('slip-appearance');
       burger.classList.add('slip-appearance');
+      burger.style.display = 'block';
     }
   });
 }
