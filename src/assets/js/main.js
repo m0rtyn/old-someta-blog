7ad7409c75978ca2aa7411e3ee07adf5a4f2f89c
martyns0n <zogacc@gmail.com>
2019-01-20 21:47:06 +0300

    make awesome main navigation on mobile devices

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -3,21 +3,30 @@
 const header = document.getElementById('header');
 const nav = document.getElementById('nav');
 const burger = document.getElementById('burger');
+const closeMenu = document.getElementById('close-menu');
+
 
 if (window.outerWidth < 500) {
   setTimeout(() => {
     burger.classList.add('slip-appearance');
   }, 1300);
 
-  document.addEventListener('click', (evt) => {
-    if (header.contains(evt.target)) {
-      nav.classList.add('slip-appearance');
-      burger.classList.remove('slip-appearance');
-      burger.style.display = 'none';
-    } else {
-      nav.classList.remove('slip-appearance');
-      burger.classList.add('slip-appearance');
-      burger.style.display = 'block';
-    }
+  burger.addEventListener('click', (evt) => {
+    // if (header.contains(evt.target)) {
+    nav.classList.add('slip-appearance');
+    burger.classList.remove('slip-appearance');
+    burger.style.display = 'none';
+    // } else {
+    //   nav.classList.remove('slip-appearance');
+    //   burger.classList.add('slip-appearance');
+    //   burger.style.display = 'block';
+    // }
+  });
+
+  closeMenu.addEventListener('click', () => {
+    console.log('close');
+    nav.classList.remove('slip-appearance');
+    burger.classList.add('slip-appearance');
+    burger.style.display = 'block';
   });
 }
