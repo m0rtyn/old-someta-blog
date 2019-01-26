f6efecc677bfeec143556c19eef0dabb9d73143d
martyns0n <zogacc@gmail.com>
2019-01-26 21:00:29 +0300

    clean code

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -12,19 +12,12 @@ if (window.outerWidth < 500) {
   }, 1300);
 
   burger.addEventListener('click', (evt) => {
-    // if (header.contains(evt.target)) {
     nav.classList.add('slip-appearance');
     burger.classList.remove('slip-appearance');
     burger.style.display = 'none';
-    // } else {
-    //   nav.classList.remove('slip-appearance');
-    //   burger.classList.add('slip-appearance');
-    //   burger.style.display = 'block';
-    // }
   });
 
   closeMenu.addEventListener('click', () => {
-    console.log('close');
     nav.classList.remove('slip-appearance');
     burger.classList.add('slip-appearance');
     burger.style.display = 'block';
