76152a2c2305a331eec854a0dd410831864212d1
martyns0n <zogacc@gmail.com>
2019-05-08 17:42:24 +0300

    add date and tags parcing

--- posts-json-generator.js
+++ posts-json-generator.js
@@ -4,7 +4,7 @@ const dirTree = require('directory-tree');
 
 const posts = [];
 const tree = dirTree('./public/posts/', {}, null, (item, PATH, stats) => {
-  console.log(item.children);
+  // console.log(item.children);
   item.children.map((element) => {
     let title = '';
     fs.readFile(element.path, 'utf8', (err, data) => {
@@ -18,11 +18,16 @@ const tree = dirTree('./public/posts/', {}, null, (item, PATH, stats) => {
       // console.log(description[0]);
       element.description = description[0];
 
-      const date = data.match(/(?!<!-- date: ).*/) || [];
-      element.date = date[0];
+      const date = data.match(/<!--date:([a-z0-9.\s]*)/i) || [];
+      element.date = date[1];
+      console.log('date:', date[1]);
 
-      const tags = data.match(/(?!<!-- tags: ).*/) || [];
-      element.tags = tags[0];
+      const tag = data.match(/<!--tags:([a-z0-9]*)/i) || [];
+      element.tag = tag[1];
+      console.log('tag:', tag[1]);
+
+      const isPublished = !!data.match(/<!--published/i);
+      element.isPublished = isPublished;
 
       posts.push(element);
     });
