3bfd27e4b4e008b46602ed2be2a8a2a0fbf82472
martyns0n <zogacc@gmail.com>
2019-05-08 17:43:02 +0300

    add date and tags parcing

--- src/assets/js/posts.js
+++ src/assets/js/posts.js
@@ -4,10 +4,16 @@ const appendContent = (data) => {
   const postPreviews = [...document.getElementsByClassName('post-item')];
   // console.log(postPreviews);
   postPreviews.map((el, i) => {
+    if (data[i].isPublished === false) return;
+
     const postLink = document.createElement('A');
     const postDescr = document.createElement('P');
+    const postDate = document.createElement('TIME');
+    const postTag = document.createElement('SPAN');
     const title = data[i].title.replace(/\d+_/g, '');
     const postPath = data[i].path.replace(/public\//g, '');
+    // const { date } = data[i];
+    // const { tag } = data[i];
 
     postLink.classList.add('post-link');
     postLink.href = postPath;
@@ -16,8 +22,16 @@ const appendContent = (data) => {
     postDescr.classList.add('post-descr');
     postDescr.innerHTML = data[i].description;
 
+    postDate.classList.add('post-date');
+    postDate.innerHTML = data[i].date;
+
+    postTag.classList.add('post-tag');
+    postTag.innerHTML = data[i].tag;
+
     el.appendChild(postLink);
     el.appendChild(postDescr);
+    el.appendChild(postDate);
+    el.appendChild(postTag);
   });
 };
 
