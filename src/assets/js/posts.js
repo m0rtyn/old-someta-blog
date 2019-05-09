ec415ff17c189aecc3387722aeaf09aeb507f402
martyns0n <zogacc@gmail.com>
2019-05-09 21:04:56 +0300

    fix posts parsing from json

--- src/assets/js/posts.js
+++ src/assets/js/posts.js
@@ -1,55 +1,55 @@
 /* eslint-env browser */
 
-const appendContent = (data) => {
-  const postPreviews = [...document.getElementsByClassName('post-item')];
-  // console.log(postPreviews);
-  postPreviews.map((el, i) => {
+const createPosts = (data) => {
+  const container = document.getElementById('posts-previews');
+  const postList = document.createElement('UL');
+
+  data.map((el, i) => {
     if (data[i].isPublished === false) return;
 
+    const postItem = document.createElement('LI');
     const postLink = document.createElement('A');
+    const postTitle = document.createElement('H3');
     const postDescr = document.createElement('P');
     const postDate = document.createElement('TIME');
     const postTag = document.createElement('SPAN');
     const title = data[i].title.replace(/\d+_/g, '');
     const postPath = data[i].path.replace(/public\//g, '');
-    // const { date } = data[i];
-    // const { tag } = data[i];
+
+    postItem.classList.add('post-item');
 
     postLink.classList.add('post-link');
     postLink.href = postPath;
-    postLink.innerHTML = title;
+
+    postTitle.classList.add('post-title');
+    postTitle.innerHTML = title;
 
     postDescr.classList.add('post-descr');
-    postDescr.innerHTML = data[i].description;
+    postDescr.innerHTML = `${data[i].description}..`;
 
     postDate.classList.add('post-date');
+    postDate.dateTime = data[i].date.replace(/(\d{1,2})\.(\d{1,2})\.(\d{4})/g, '$3-$2-$1');
     postDate.innerHTML = data[i].date;
 
     postTag.classList.add('post-tag');
-    postTag.innerHTML = data[i].tag;
+    postTag.innerHTML = data[i].tag ? `#${data[i].tag}` : '';
 
-    el.appendChild(postLink);
-    el.appendChild(postDescr);
-    el.appendChild(postDate);
-    el.appendChild(postTag);
-  });
-};
+    setTimeout(() => {
+      postLink.appendChild(postTitle);
+      postLink.appendChild(postDescr);
+      postLink.appendChild(postDate);
+      postLink.appendChild(postTag);
 
-const createPreviews = (data) => {
-  const container = document.getElementById('posts-previews');
-  const postList = document.createElement('UL');
-  data.map(() => {
-    const postWrapper = document.createElement('LI');
-    postWrapper.classList.add('post-item');
-    return postList.appendChild(postWrapper);
+      postItem.appendChild(postLink);
+      postList.appendChild(postItem);
+    }, 1);
   });
+
   container.appendChild(postList);
-  return data;
 };
 
 fetch(
   '/posts.json',
 )
   .then(response => response.json())
-  .then(createPreviews)
-  .then(appendContent);
+  .then(createPosts);
