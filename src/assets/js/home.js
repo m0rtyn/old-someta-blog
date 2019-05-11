2533a5e14e73d49c7237313dcfe4df21fef7cbe2
martyns0n <zogacc@gmail.com>
2019-05-11 08:33:17 +0300

    comment unusable code

--- src/assets/js/home.js
+++ src/assets/js/home.js
@@ -1,13 +1,13 @@
-window.onload = () => {
-  const linksOverVideos = document.getElementsByClassName('article-link');
-  const vids = document.getElementsByTagName('video');
+// window.onload = () => {
+//   const linksOverVideos = document.getElementsByClassName('article-link');
+//   const vids = document.getElementsByTagName('video');
 
-  for (let i = 0; i < vids.length; i++) {
-    linksOverVideos[i].addEventListener('mouseover', () => {
-      vids[i].play();
-    });
-    linksOverVideos[i].addEventListener('mouseout', () => {
-      vids[i].pause();
-    });
-  }
-};
+//   for (let i = 0; i < vids.length; i++) {
+//     linksOverVideos[i].addEventListener('mouseover', () => {
+//       vids[i].play();
+//     });
+//     linksOverVideos[i].addEventListener('mouseout', () => {
+//       vids[i].pause();
+//     });
+//   }
+// };
