148d27b2edcc43f81fab23e8e67626d43cb74615
martyns0n <zogacc@gmail.com>
2019-05-12 20:46:05 +0300

    fix broken order of posts

--- posts-json-generator.js
+++ posts-json-generator.js
@@ -3,37 +3,37 @@ const fs = require('fs');
 const dirTree = require('directory-tree');
 
 const posts = [];
-const tree = dirTree('./public/posts/', {}, null, (item, PATH, stats) => {
-  // console.log(item.children);
+const tree = dirTree('./public/posts/', {}, null, (item) => {
+  // console.log(item);
   item.children.map((element) => {
+    // console.log(element.name);
     let title = '';
     fs.readFile(element.path, 'utf8', (err, data) => {
       if (err) throw err;
 
       title = data.match(/(?!<h1>)[а-яa-z0-9\s#]+(?=<\/h1>)/ig) || [];
-      // console.log(title);
       element.title = title[0];
 
       const description = data.match(/(?!<p>)([a-zа-я0-9ё.—\-/,«»:\s]+\n?)(?=<\/p>)/i) || [];
-      // console.log(description[0]);
       element.description = description[0];
 
       const date = data.match(/<!--date:([a-z0-9.\s]*)/i) || [];
       element.date = date[1];
-      console.log('date:', date[1]);
+      // console.log('date:', date[1]);
 
-      const tag = data.match(/<!--tags:([a-z0-9]*)/i) || [];
+      const tag = data.match(/<!--tags:([a-z0-9_]*)/i) || [];
       element.tag = tag[1];
-      console.log('tag:', tag[1]);
+      // console.log('tag:', tag[1]);
 
       const isPublished = !!data.match(/<!--published/i);
       element.isPublished = isPublished;
-
-      posts.push(element);
     });
+    posts.push(element);
   });
 });
 
+
 setTimeout(() => {
+  console.log(posts);
   fs.writeFileSync('public/posts.json', JSON.stringify(posts));
 }, 1000);
