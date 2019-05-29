73d0c83066b14f33d754fae618bb1947379fa6f3
martyns0n <zogacc@gmail.com>
2019-05-29 21:09:59 +0300

    fix papers page markup

--- src/assets/js/home.js
+++ src/assets/js/home.js
@@ -1,13 +1,14 @@
-// window.onload = () => {
-//   const linksOverVideos = document.getElementsByClassName('article-link');
-//   const vids = document.getElementsByTagName('video');
+/* eslint-env browser */
+window.onload = () => {
+  const linksOverVideos = document.getElementsByClassName('paper-link');
+  const vids = document.getElementsByTagName('video');
 
-//   for (let i = 0; i < vids.length; i++) {
-//     linksOverVideos[i].addEventListener('mouseover', () => {
-//       vids[i].play();
-//     });
-//     linksOverVideos[i].addEventListener('mouseout', () => {
-//       vids[i].pause();
-//     });
-//   }
-// };
+  for (let i = 0; i < vids.length; i++) {
+    linksOverVideos[i].addEventListener('mouseover', () => {
+      vids[i].play();
+    });
+    linksOverVideos[i].addEventListener('mouseout', () => {
+      vids[i].pause();
+    });
+  }
+};
