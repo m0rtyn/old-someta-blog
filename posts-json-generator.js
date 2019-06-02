47ba94b9f1fd0daadf07a8954f48e120499f2ecc
martyns0n <zogacc@gmail.com>
2019-06-02 19:49:11 +0300

    extend posts json object with statuses and platforms

--- posts-json-generator.js
+++ posts-json-generator.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const dirTree = require('directory-tree');
 
 const posts = [];
-const tree = dirTree('./public/posts/', {}, null, (item) => {
+const tree = dirTree('./public/posts/', { exclude: /posts-kanban.html|index.html/ }, null, (item) => {
   // console.log(item);
   item.children.map((element) => {
     // console.log(element.name);
@@ -11,22 +11,25 @@ const tree = dirTree('./public/posts/', {}, null, (item) => {
     fs.readFile(element.path, 'utf8', (err, data) => {
       if (err) throw err;
 
-      title = data.match(/(?!<h1>)[а-яa-z0-9\s#]+(?=<\/h1>)/ig) || [];
-      element.title = title[0];
+      title = data.match(/(?:<h1>)([а-яa-z0-9\s#]+|.+)(?=<\/h1>)/i) || [];
+      element.title = title[1];
 
       const description = data.match(/(?!<p>)([a-zа-я0-9ё.—\-/,«»:\s]+\n?)(?=<\/p>)/i) || [];
       element.description = description[0];
 
       const date = data.match(/<!--date:([a-z0-9.\s]*)/i) || [];
       element.date = date[1];
-      // console.log('date:', date[1]);
 
       const tag = data.match(/<!--tags:([a-z0-9_]*)/i) || [];
       element.tag = tag[1];
-      // console.log('tag:', tag[1]);
 
-      const isPublished = !!data.match(/<!--published/i);
-      element.isPublished = isPublished;
+      const status = data.match(/<!--status:([a-z0-9.\s]*)/i) || [];
+      element.status = status[1];
+
+      const platforms = data.match(/(?:<!--platforms:)(.*)(-->)/i) || [];
+      element.platforms = platforms[1]
+        ? platforms[1].split(',')
+        : undefined;
     });
     posts.push(element);
   });
