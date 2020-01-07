ea07b248e8657c33fb73e7a7bd96b1dd3ad8805d
Martyn <zogacc@gmail.com>
2020-01-07 20:01:33 +0300

    remove dead code

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -1,32 +1,10 @@
 import chronoblogTheme from 'gatsby-theme-chronoblog/src/gatsby-plugin-theme-ui';
 
-// // MONOKAI theme
-// --color-dark: #2D2A2E;
-// --color-light: #FCFCFA;
-// --color-r: #FF6188;
-// --color-g: #A9DC76;
-// --color-o: #FC9867;
-// --color-c: #78DCE8;
-// --color-p: #AB9DF2;
-// --color-y: #FFD866;
-// --color-gray: #939293;
-
 export default {
   ...chronoblogTheme,
   initialColorMode: 'dark',
   colors: {
     // ...chronoblogTheme.colors,
-    // ? Monokai theme
-    dark: '#2D2A2E',
-    light: '#FCFCFA',
-    red: '#FF6188',
-    green: '#A9DC76',
-    orange: '#FC9867',
-    cyan: '#78DCE8',
-    purple: '#AB9DF2',
-    yellow: '#FFD866',
-    gray: '#939293',
-    // ? ===
     text: '#FCFCFA',
     background: '#2D2A2E',
     link: '#FFD866',
