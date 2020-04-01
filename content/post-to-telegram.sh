e9c2de08686289d80136dbd533421bdc32223cf5
Martyn <zogacc@gmail.com>
2020-04-01 09:29:43 +0300

    improve posting script

--- content/post-to-telegram.sh
+++ content/post-to-telegram.sh
@@ -1,10 +1,49 @@
 #!/bin/bash
 
+main() {
+    local CMD=$1
+    shift
+
+    case $CMD in
+        testRegExp) testRegExp "$@";;
+        publishPost) publishPost "$@";;
+        postToTelegram) postToTelegram "$@";;
+        *) echo "Run as: $0 command
+    "; exit;;
+    esac
+}
+
+testRegExp() {
+  FILE=$1
+  FILE_PATH="./_inProgress/${FILE}"
+  # FILE_CONTENTS=$(cat $FILE_PATH)
+  # echo $FILE_CONTENTS
+
+  POST_TITLE=$(grep '^title: .*$' $FILE_PATH)
+  POST_DESCRIPTION=$(grep '^description: .*$' $FILE_PATH)
+
+  POST_URL=$(grep -o '\w*/index.md' <<< $FILE)
+  echo ${POST_URL:-6}
+}
+
+publishPost() {
+  FILE=$1
+  echo $FILE
+  FILE_PATH="./_inProgress/${FILE}"
+  DEST_PATH="./posts/${FILE}"
+  
+  mv FILE_PATH DEST_PATH
+}
+
 postToTelegram() {
   FILE=$1
   echo $FILE
+  FILE_PATH="./posts/${FILE}"
+  # POST_TITLE=
+  # POST_DESC=
 
-  cat "./_inProgress/${FILE}" | https_proxy=https://151.253.165.70:8080 telegram-send --timeout 50.0 --format markdown --stdin
+  cat FILE_PATH | 
+  https_proxy=https://151.253.165.70:8080 telegram-send --timeout 50.0 --format markdown --stdin
 }
 
-postToTelegram "$@"
+main "$@"
