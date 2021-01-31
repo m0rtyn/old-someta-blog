a20ea7ba8af6441dc733372a9497a955b20f5db2
Martyn <zogacc@gmail.com>
2021-01-31 20:37:56 +0200

    change home page

---
+++ src/pages/index.jsx
@@ -0,0 +1,57 @@
+/** @jsx jsx */
+import React from 'react';
+import { jsx } from 'theme-ui';
+import { graphql } from 'gatsby';
+import ProjectBanner from 'components/ProjectBanner';
+import BlogItems from 'components/BlogItems';
+import Tags from 'gatsby-theme-chronoblog/src/components/tags';
+import Layout from 'gatsby-theme-chronoblog/src/components/layout';
+
+// import FeedItems from 'gatsby-theme-chronoblog/src/components/feed-items';
+
+const HomePage = ({ data }) => {
+  const { allPosts } = data;
+
+  return (
+    <Layout>
+      <ProjectBanner />
+      {/* <Tags /> */}
+      <BlogItems posts={allPosts} />
+
+      {/* 
+        // old blog post local source
+        <FeedItems
+          filter={(item) => !item.frontmatter.tags.includes('personal')}
+        />
+      */}
+    </Layout>
+  );
+};
+
+export default HomePage;
+
+export const query = graphql`
+  query {
+    allPosts(
+      filter: {
+        status: { eq: "published" }
+        content_type: { eq: "article" }
+      }
+      sort: { fields: [publish_date___startDate], order: DESC }
+    ) {
+      nodes {
+        name
+        tags
+        desc
+        content_type
+        status
+        url
+        slug
+        cover_image
+        publish_date {
+          startDate(formatString: "DD MMM YYYY", fromNow: false)
+        }
+      }
+    }
+  }
+`;
