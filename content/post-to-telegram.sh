735f88b51b2f4c3797a98185cfda5879f64df0b2
Martyn <zogacc@gmail.com>
2021-01-31 20:34:01 +0200

    move local articles to archive

--- content/post-to-telegram.sh
+++ content/post-to-telegram.sh
@@ -1,28 +1,33 @@
 #!/bin/bash
 
 main() {
-    local CMD=$1
-    shift
+    # local CMD=$1
+    
+    publish "$@" # > ./post-to-telegram.sh publish folder/file.ext
 
-    case $CMD in
-        publishToTelegram) publishToTelegram "$@";;
-        *) echo "Run as: $0 command
-    "; exit;;
-    esac
+    # shift
+    # case $CMD in
+    #     # > ./post-to-telegram.sh publish folder/file.ext
+    #     publishToTelegram) publish "$@";;
+    #     *) echo "Run as: $0 command
+    # "; exit;;
+    # esac
 }
 
-publishToTelegram() {
+publish() {
   FILE_NAME=$1
-  echo $FILE_NAME
+  echo "file name: $FILE_NAME"
   FILE_PATH="./_inProgress/${FILE_NAME}"
-  echo $FILE_PATH
-  TEMP_PATH="./temp.md"
+  echo "file path: $FILE_PATH"
+
+  TEMP_PATH="./temp.md" # node script will write text to temp file
   
   node postToTelegram.js $FILE_PATH
 
   # cat FILE_PATH | 
   # https_proxy=https://151.253.165.70:8080 telegram-send --timeout 50.0 --format markdown --stdin
-  cat TEMP_PATH | telegram-send --timeout 50.0 --format markdown --stdin
+
+  # cat TEMP_PATH | telegram-send --timeout 50.0 --format markdown --stdin
 }
 
 main "$@"
--- content/postToTelegram.js
+++ content/postToTelegram.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 
-const filePath = process.argv.slice(2, 3)[0];
+// call by shell script as `node postToTelegram.js $FILE_PATH`
+const filePath = process.argv[2];
 
 const generatePostContent = postName => {
   const url = `https://someta.site/${postName}/`;
@@ -20,19 +21,24 @@ fs.readFile(filePath, 'utf8', (err, data) => {
   );
   const postName = postNameString[1];
 
-  const title = data.match(/title: (.*)/)[1].toUpperCase();
-  const textWithoutMetaData = data.replace(/---(\n.*)+---/g, '');
+  const title = data.match(/title: (.*)/)[1].toUpperCase(); // replace title and MAKE BIG
+
+  const textWithoutMetaData = data.replace(/---(\n.*)+---/g, ''); // remove metadata
+
   const formattedText = textWithoutMetaData.replace(
     /## (.+)/g,
     '**$1**'
-  );
+  ); // replace markdown titles by bold text
 
-  const isLongText = formattedText.length > 600;
+  // const isLongText = formattedText.length > 600;
 
-  const dest = isLongText ? 'posts/' : 'notes/';
-  const content = isLongText
-    ? generatePostContent(postName)
-    : generateNoteContent(formattedText, title);
+  // const destFolder = isLongText ? 'posts' : 'notes';
+  const destFolder = 'notes';
+
+  const content = generateNoteContent(formattedText, title);
+  // const content = isLongText
+  //   ? generatePostContent(postName)
+  //   : generateNoteContent(formattedText, title);
 
   fs.writeFile('./temp.md', content, 'utf8', e => {
     if (e) throw e;
@@ -40,15 +46,23 @@ fs.readFile(filePath, 'utf8', (err, data) => {
     console.info('The file has been writed to temp folder!');
   });
 
-  fs.writeFile(`${dest}${postName}.md`, data, 'utf8', e => {
+  const destPath = `./${destFolder}/${postName}.md`;
+
+  fs.appendFile(destPath, data, 'utf-8', e => {
     if (e) throw e;
 
     console.info('The file has been replaced to published texts!');
   });
 
-  fs.unlink(filePath, e => {
+  fs.writeFile(destPath, data, 'utf8', e => {
     if (e) throw e;
 
-    console.info('The file was deleted');
+    console.info('The file has been replaced to published texts!');
   });
+
+  // fs.unlink(filePath, e => {
+  //   if (e) throw e;
+
+  //   console.info('The file was deleted');
+  // });
 });
