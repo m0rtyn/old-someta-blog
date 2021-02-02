12ac1461baca5af9281fa578377aacc5c33561d3
Martyn <zogacc@gmail.com>
2021-02-02 22:55:08 +0200

    add key generation

--- src/components/BlogItems.jsx
+++ src/components/BlogItems.jsx
@@ -3,15 +3,18 @@
 import React from 'react';
 import { jsx } from 'theme-ui';
 import { BlogItem } from 'components/blog-item';
+import { uid } from 'react-uid';
 
 const BlogItems = ({ items }) => {
   if (!items) return null;
 
   return (
     <>
-      {items.nodes.map(node => (
-        <BlogItem item={node} />
-      ))}
+      {items.nodes.map(node => {
+        const key = uid(node);
+
+        return <BlogItem key={key} item={node} />;
+      })}
     </>
   );
 };
