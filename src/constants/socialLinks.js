f85212b7811d3f48e7df77ba4c127ee52d5e6b8a
Martyn <zogacc@gmail.com>
2021-05-03 13:03:26 +0200

    change username in links

--- src/constants/socialLinks.js
+++ src/constants/socialLinks.js
@@ -1,17 +1,17 @@
 const socialLinks = [
   {
     icon: `telegram`,
-    url: `https://t.me/martyns0n`,
+    url: `https://t.me/m0rtyn`,
     altText: `Телеграм Мартына`
   },
   {
     icon: `vk`,
-    url: `https://vk.com/martyns0n`,
+    url: `https://vk.com/m0rtyn`,
     altText: `Вконтакт Мартына`
   },
   {
     icon: `github`,
-    url: `https://github.com/martyns0n`,
+    url: `https://github.com/m0rtyn`,
     altText: `Гитхаб Мартына`
   }
 ];
